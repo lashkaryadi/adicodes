3b8a1b8ecfaf84958c7702dae2203a9d825186a7
ADITYA <lashkary.adi@gmail.com>
2025-07-30 10:32:59 +0530

    Update revision-iii.cpp

--- revision-iii.cpp
+++ revision-iii.cpp
@@ -12,14 +12,14 @@ using namespace std;
     }
 }*/
 
-/*void ascend(int n){
+void ascend(int n){
     for (int i=0;i<n;i++){
         for (int j=0;j<=i;j++){
             cout << "*";
         }
         cout << endl;
     }
-}*/
+}
 
 /*void ascend_no(int n){
     for (int i=0;i<n;i++){
@@ -39,14 +39,14 @@ using namespace std;
     }
 }*/
 
-/*void descend(int n){
+void descend(int n){
  for(int row=0;row<n;row++){
     for (int col=n-row;col>0;col--){
         cout << "*";
     }
     cout << endl;
  }
-}*/
+}
 
 /*void descend_col(int n){
     for (int row=0;row<n;row++){
@@ -93,9 +93,80 @@ void ascend_binary(int n){
     }
 }
 
+void pyramid(int n){
+    for (int i=0;i<n;i++){
+        for (int j=0;j<=i;j++){
+            cout << j+1;
+        }
+        for( int j=0;j<2*(n-i-1);j++){
+            cout << " ";
+        }
+        for (int j=0;j<=i;j++){
+            cout << i+1-j;
+        }
+        cout << endl;
+    }
+}
+
+void pyramid_ch(int n){
+    for (int i=0;i<n;i++){
+        for (int space = 0; space < n-i-1;space++ ){
+            cout << " ";
+        }
+        char ch='A';
+        for (int j=0;j<(2*i)+1;j++){
+            cout << ch;
+            if (j<i)
+            ch++;
+            else
+            ch--;
+        }
+        cout << endl;
+    }
+}
+
+void diamond(int n){
+    for(int row=0;row<n;row++){
+    for (int col=n-row;col>0;col--){
+        cout << "*";
+    }
+    for (int space=0; space< 2*row; space++){
+        cout << " ";
+    }
+    for (int col=n-row;col>0;col--){
+        cout << "*";
+    }
+    cout << endl;
+    }
+
+    for(int row=0;row<n;row++){
+    for (int col=0;col<=row;col++){
+        cout << "*";
+    }
+    for (int space=0; space< 2*(n-row-1); space++){
+        cout << " ";
+    }
+    for (int col=0;col<=row;col++){
+        cout << "*";
+    }
+    cout << endl;
+    }
+}
+
+void square(int n){
+    for(int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            if (i==0||j==0||i==n-1||j==n-1)
+            cout << "* ";
+            else
+            cout << "  ";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
     int n;
     cin >> n;
-    ascend_binary(n);
+    square(n);
 }
